ed6dbf5e6f8b26b9cbbcab2ec429d6a199059920
Nimra Talha <nimratalha@gmail.com>
2024-12-10 00:28:30 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Footer from "@/app/Components/Footer";
+import Footer from "./Components/Footer";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import Benefit from "./Components/Benefits";
+import Brand from "./Components/Brand";
+import Ceramics from "./Components/Ceramics";
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
+import Product from "./Components/Products";
+import Touch from "./Components/Touch";
+import Footer from "./Components/Footer"
+
+
+
+
+export default function home() {
+  return (
+   <>
+   <Navbar/>
+   <Hero />
+   <Brand />
+   <Ceramics />
+   <Product />
+   <Benefit />
+   <Touch />
+   <Footer/>
+
+   
+
+   </>
+  );
+}
